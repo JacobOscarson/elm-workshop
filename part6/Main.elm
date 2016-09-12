f3d0457277375182545d5e3ae4b91741925f5687
Jacob Oscarson <jacob@plexical.com>
2016-09-12 20:12:58 +0200

    Completed part 6 of the workshop

--- part6/Main.elm
+++ part6/Main.elm
@@ -25,11 +25,10 @@ searchResultDecoder =
     --
     -- Look in SampleResponse.elm to see the exact JSON we'll be decoding!
     --
-    -- TODO replace these calls to `hardcoded` with calls to `required`
     decode SearchResult
-        |> hardcoded 0
-        |> hardcoded ""
-        |> hardcoded 0
+        |> required "id" int
+        |> required "full_name" string
+        |> required "stargazers_count" int
 
 
 type alias Model =
@@ -61,20 +60,11 @@ responseDecoder =
 decodeResults : String -> List SearchResult
 decodeResults json =
     case decodeString responseDecoder json of
-        -- TODO add branches to this case-expression which return:
-        --
-        -- * the search results, if decoding succeeded
-        -- * an empty list if decoding failed
-        --
-        -- see http://package.elm-lang.org/packages/elm-lang/core/4.0.0/Json-Decode#decodeString
-        --
-        -- HINT: decodeString returns a Result which is one of the following:
-        --
-        -- Ok (List SearchResult)
-        -- Err String
-        _ ->
-            []
+        Ok items ->
+            items
 
+        Err message ->
+            []
 
 view : Model -> Html Msg
 view model =
